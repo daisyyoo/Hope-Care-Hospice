989387b15247b24f179146ede96c10a0595131cf
Daisy Yoo <daisyhyoo@gmail.com>
2023-01-11 23:49:12 +0000

    Added schema for database

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,12 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."patients" (
+	"name" TEXT NOT NULL,
+	"phone" int NOT NULL,
+	"email" TEXT NOT NULL,
+	"comments" TEXT NOT NULL
+) WITH (
+  OIDS=FALSE
+);
