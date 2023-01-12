45a732910afc31e693a01b966de5405e05033e88
Daisy Yoo <daisyhyoo@gmail.com>
2023-01-12 00:09:20 +0000

    Testing for server backend functional to add newInquiry info

--- server/index.js
+++ server/index.js
@@ -1,14 +1,36 @@
 require('dotenv/config');
 const express = require('express');
+const pg = require('pg');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
 
 const app = express();
 
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.use(express.json());
 app.use(staticMiddleware);
 
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.post('/newInquiry', (req, res, next) => {
+  const { name, phone, email, comment } = req.body;
+
+  const sql = `
+    insert into "patients" ("name", "phone", "email", "comment")
+    values ($1, $2, $3, $4)
+    returning *
+  `;
+  const params = [name, phone, email, comment];
+  db.query(sql, params)
+    .then(result => {
+      const newPatient = result.rows;
+      res.status(201).json(newPatient);
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
