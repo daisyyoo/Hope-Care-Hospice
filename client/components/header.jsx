a77d2b01f776a4cc4be44241ed2c4600947feb97
Daisy Yoo <daisyhyoo@gmail.com>
2023-01-25 00:53:59 +0000

    Fixed header for medium and large breakpoints. Also added tel phone number for phone number button

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -25,17 +25,17 @@ export default class Header extends React.Component {
             <Navbar.Toggle aria-controls="offcanvasNavbar-expand"/>
           </div>
           <Navbar.Offcanvas id="offcanvasNavbar-expand" aria-labelledby="offcanvasNavbar-expand" placement="end">
-            <Offcanvas.Header className="mx-4 border-bot" closeButton>
+            <Offcanvas.Header className="mx-4" closeButton>
               <Offcanvas.Title className="pt-2" id="offcanvasNavbar-expand">
                 Hope Care Hospice
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body className="px-4">
               <Nav className="justify-content-end flex-grow-1 pe-3" onSelect={(_, event) => { window.location.href = event.target.href; }} >
-                <Button className="px-3 px-lg-3 nav-button" href="">714.724.8033</Button>
-                <Nav.Link className="px-3 px-lg-3 menu-link" href="#">HOSPICE BASICS</Nav.Link>
-                <Nav.Link className="px-3 px-lg-3 menu-link" href="#newInquiry">REFERRALS</Nav.Link>
-                <Nav.Link className="px-3 px-lg-3 menu-link" href="#aboutUs">ABOUT US</Nav.Link>
+                <Button className=" px-md-3 call-button-all" href="tel:+1-714-724-8033">714.724.8033</Button>
+                <Nav.Link className=" px-md-3 menu-link" href="#hospiceBasics">HOSPICE BASICS</Nav.Link>
+                <Nav.Link className=" px-md-3 menu-link" href="#newInquiry">REFERRALS</Nav.Link>
+                <Nav.Link className=" px-md-3 menu-link" href="#aboutUs">ABOUT US</Nav.Link>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
